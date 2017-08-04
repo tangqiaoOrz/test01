586c493660253737cf529a08b0c3302d7956ca7c
tangqiaoOrz <30646722+tangqiaoOrz@users.noreply.github.com>
2017-08-04 15:24:47 +0800

    Create hellojava.java

---
+++ hellojava.java
@@ -0,0 +1,8 @@
+public class HelloWorld  
+{  
+	public static void main(string[] args)  
+	{  
+	System.out.println("Hello Javajavajavajava!");  
+	System.out.println("This is the first use");
+	}  
+}  
